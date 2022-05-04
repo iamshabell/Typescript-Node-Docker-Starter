ee8645da442bbfb018f69acb45197ecb8b8d07eb
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-05-04 15:35:16 +0300

    chore(setup): add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on port ${port}`);
+});
