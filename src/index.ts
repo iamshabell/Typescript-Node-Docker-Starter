75bc762b80b629fc19f76815617db4a2ee152117
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-05-14 21:31:10 +0300

    chore(setup): Testing husky

--- src/index.ts
+++ src/index.ts
@@ -37,5 +37,5 @@ app.get('/', async (req, res) => {
 
 const port = Number(process.env.PORT ?? 8080);
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at:  http://localhost:${port}`);
 });
